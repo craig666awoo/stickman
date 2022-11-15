4fb53664a08e0ab950ff750dc1f7eddccfc6b58e
craig666awoo <114919156+craig666awoo@users.noreply.github.com>
2022-11-15 10:06:21 +0300

    Add files via upload

---
+++ stickman.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<" O\n";
+    cout<<"/|\\\n";
+    cout<<"/ \\\n\n";
+    cout<<" O\n";
+    cout<<"/|\\\n";
+    cout<<"/_\\\n";
+    cout<<"| |";
+
+}
